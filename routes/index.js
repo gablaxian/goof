00e4961ab5a026985b0d0f18778a34083be75992
Graham Smith <g@blaxian.com>
2023-03-03 14:33:17 +0000

    chore: adds new code vuln

--- routes/index.js
+++ routes/index.js
@@ -19,11 +19,13 @@ var fs = require('fs');
 // prototype-pollution
 var _ = require('lodash');
 
+var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
+console.log('token: ' + token);
+
 exports.index = function (req, res, next) {
-  Todo.
-    find({}).
-    sort('-updated_at').
-    exec(function (err, todos) {
+  Todo.find({})
+    .sort('-updated_at')
+    .exec(function (err, todos) {
       if (err) return next(err);
 
       res.render('index', {
@@ -36,31 +38,34 @@ exports.index = function (req, res, next) {
 
 exports.loginHandler = function (req, res, next) {
   if (validator.isEmail(req.body.username)) {
-    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
-      if (users.length > 0) {
-        const redirectPage = req.body.redirectPage
-        const session = req.session
-        const username = req.body.username
-        return adminLoginSuccess(redirectPage, session, username, res)
-      } else {
-        return res.status(401).send()
+    User.find(
+      { username: req.body.username, password: req.body.password },
+      function (err, users) {
+        if (users.length > 0) {
+          const redirectPage = req.body.redirectPage;
+          const session = req.session;
+          const username = req.body.username;
+          return adminLoginSuccess(redirectPage, session, username, res);
+        } else {
+          return res.status(401).send();
+        }
       }
-    });
+    );
   } else {
-    return res.status(401).send()
+    return res.status(401).send();
   }
 };
 
 function adminLoginSuccess(redirectPage, session, username, res) {
-  session.loggedIn = 1
+  session.loggedIn = 1;
 
   // Log the login action for audit
-  console.log(`User logged in: ${username}`)
+  console.log(`User logged in: ${username}`);
 
   if (redirectPage) {
-      return res.redirect(redirectPage)
+    return res.redirect(redirectPage);
   } else {
-      return res.redirect('/admin')
+    return res.redirect('/admin');
   }
 }
 
@@ -68,7 +73,7 @@ exports.login = function (req, res, next) {
   return res.render('admin', {
     title: 'Admin Access',
     granted: false,
-    redirectPage: req.query.redirectPage
+    redirectPage: req.query.redirectPage,
   });
 };
 
@@ -79,53 +84,54 @@ exports.admin = function (req, res, next) {
   });
 };
 
-exports.get_account_details = function(req, res, next) {
+exports.get_account_details = function (req, res, next) {
   // @TODO need to add a database call to get the profile from the database
   // and provide it to the view to display
-  const profile = {}
- 	return res.render('account.hbs', profile)
-}
+  const profile = {};
+  return res.render('account.hbs', profile);
+};
 
-exports.save_account_details = function(req, res, next) {
+exports.save_account_details = function (req, res, next) {
   // get the profile details from the JSON
-	const profile = req.body
+  const profile = req.body;
   // validate the input
-  if (validator.isEmail(profile.email, { allow_display_name: true })
+  if (
+    validator.isEmail(profile.email, { allow_display_name: true }) &&
     // allow_display_name allows us to receive input as:
     // Display Name <email-address>
     // which we consider valid too
-    && validator.isMobilePhone(profile.phone, 'he-IL')
-    && validator.isAscii(profile.firstname)
-    && validator.isAscii(profile.lastname)
-    && validator.isAscii(profile.country)
+    validator.isMobilePhone(profile.phone, 'he-IL') &&
+    validator.isAscii(profile.firstname) &&
+    validator.isAscii(profile.lastname) &&
+    validator.isAscii(profile.country)
   ) {
     // trim any extra spaces on the right of the name
-    profile.firstname = validator.rtrim(profile.firstname)
-    profile.lastname = validator.rtrim(profile.lastname)
+    profile.firstname = validator.rtrim(profile.firstname);
+    profile.lastname = validator.rtrim(profile.lastname);
 
     // render the view
-    return res.render('account.hbs', profile)
+    return res.render('account.hbs', profile);
   } else {
     // if input validation fails, we just render the view as is
-    console.log('error in form details')
-    return res.render('account.hbs')
+    console.log('error in form details');
+    return res.render('account.hbs');
   }
-}
+};
 
 exports.isLoggedIn = function (req, res, next) {
   if (req.session.loggedIn === 1) {
-    return next()
+    return next();
   } else {
-    return res.redirect('/')
+    return res.redirect('/');
   }
-}
+};
 
 exports.logout = function (req, res, next) {
-  req.session.loggedIn = 0
-  req.session.destroy(function() { 
-    return res.redirect('/')  
-  })
-}
+  req.session.loggedIn = 0;
+  req.session.destroy(function () {
+    return res.redirect('/');
+  });
+};
 
 function parse(todo) {
   var t = todo;
@@ -154,7 +160,7 @@ exports.create = function (req, res, next) {
 
   var item = req.body.content;
   var imgRegex = /\!\[alt text\]\((http.*)\s\".*/;
-  if (typeof (item) == 'string' && item.match(imgRegex)) {
+  if (typeof item == 'string' && item.match(imgRegex)) {
     var url = item.match(imgRegex)[1];
     console.log('found img: ' + url);
 
@@ -164,7 +170,6 @@ exports.create = function (req, res, next) {
         console.log('Error (' + err + '):' + stderr);
       }
     });
-
   } else {
     item = parse(item);
   }
@@ -189,35 +194,31 @@ exports.create = function (req, res, next) {
 
 exports.destroy = function (req, res, next) {
   Todo.findById(req.params.id, function (err, todo) {
-
     try {
       todo.remove(function (err, todo) {
         if (err) return next(err);
         res.redirect('/');
       });
-    } catch (e) {
-    }
+    } catch (e) {}
   });
 };
 
 exports.edit = function (req, res, next) {
-  Todo.
-    find({}).
-    sort('-updated_at').
-    exec(function (err, todos) {
+  Todo.find({})
+    .sort('-updated_at')
+    .exec(function (err, todos) {
       if (err) return next(err);
 
       res.render('edit', {
         title: 'TODO',
         todos: todos,
-        current: req.params.id
+        current: req.params.id,
       });
     });
 };
 
 exports.update = function (req, res, next) {
   Todo.findById(req.params.id, function (err, todo) {
-
     todo.content = req.body.content;
     todo.updated_at = Date.now();
     todo.save(function (err, todo, count) {
@@ -230,12 +231,11 @@ exports.update = function (req, res, next) {
 
 // ** express turns the cookie key to lowercase **
 exports.current_user = function (req, res, next) {
-
   next();
 };
 
 function isBlank(str) {
-  return (!str || /^\s*$/.test(str));
+  return !str || /^\s*$/.test(str);
 }
 
 exports.import = function (req, res, next) {
@@ -247,15 +247,15 @@ exports.import = function (req, res, next) {
   var importFile = req.files.importFile;
   var data;
   var importedFileType = fileType(importFile.data);
-  var zipFileExt = { ext: "zip", mime: "application/zip" };
+  var zipFileExt = { ext: 'zip', mime: 'application/zip' };
   if (importedFileType === null) {
-    importedFileType = { ext: "txt", mime: "text/plain" };
+    importedFileType = { ext: 'txt', mime: 'text/plain' };
   }
-  if (importedFileType["mime"] === zipFileExt["mime"]) {
+  if (importedFileType['mime'] === zipFileExt['mime']) {
     var zip = AdmZip(importFile.data);
-    var extracted_path = "/tmp/extracted_files";
+    var extracted_path = '/tmp/extracted_files';
     zip.extractAllTo(extracted_path, true);
-    data = "No backup.txt file found";
+    data = 'No backup.txt file found';
     fs.readFile('backup.txt', 'ascii', function (err, data) {
       if (!err) {
         data = data;
@@ -297,12 +297,11 @@ exports.import = function (req, res, next) {
 
 exports.about_new = function (req, res, next) {
   console.log(JSON.stringify(req.query));
-  return res.render("about_new.dust",
-    {
-      title: 'Patch TODO List',
-      subhead: 'Vulnerabilities at their best',
-      device: req.query.device
-    });
+  return res.render('about_new.dust', {
+    title: 'Patch TODO List',
+    subhead: 'Vulnerabilities at their best',
+    device: req.query.device,
+  });
 };
 
 // Prototype Pollution
@@ -321,9 +320,9 @@ let messages = [];
 let lastId = 1;
 
 function findUser(auth) {
-  return users.find((u) =>
-    u.name === auth.name &&
-    u.password === auth.password);
+  return users.find(
+    (u) => u.name === auth.name && u.password === auth.password
+  );
 }
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -363,5 +362,5 @@ exports.chat = {
 
     messages = messages.filter((m) => m.id !== req.body.messageId);
     res.send({ ok: true });
-  }
+  },
 };
